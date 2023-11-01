c5d7322f66ea8117ff79fb5f3904d6c06da9f0a1
kaif6092 <kaifadnan3022@gmail.com>
2023-11-01 12:59:47 +0000

    while loop +if statement

--- src/Program.cs
+++ src/Program.cs
@@ -8,6 +8,7 @@
         int helmetPrice = 3000;
         int foodPrice = 500;
         int presentPrice = 1000;
+        int milkshakePrice = 700; 
 
         Console.WriteLine("------------------------------");
         Console.WriteLine("Welcome to the general store!");
@@ -15,12 +16,15 @@
 
         Console.WriteLine("");
 
-        Console.WriteLine("What would you like to purchase?");
+        while (playerCoins >= 0)         
+        {
+          Console.WriteLine("What would you like to purchase?");
 
         Console.WriteLine("A: A nice shirt.");
         Console.WriteLine("B: A protective helmet.");
         Console.WriteLine("C: Some food.");
         Console.WriteLine("D: A present.");
+        Console.WriteLine("E: A milkshake.");
 
         Console.WriteLine("");
 
@@ -29,24 +33,57 @@
 
         Console.WriteLine("");
 
+        var shop = new Shop();
+
         switch (purchaseLetter) {
             case "A":
-                playerCoins = playerCoins - shirtPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                playerCoins = shop.chargeForItem(playerCoins, shirtPrice);
                 break;
             case "B":
-                playerCoins = playerCoins - helmetPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                playerCoins = shop.chargeForItem(playerCoins, helmetPrice);
                 break;
             case "C":
-                playerCoins = playerCoins - foodPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                playerCoins = shop.chargeForItem(playerCoins, foodPrice);
                 break;
             case "D":
-                playerCoins = playerCoins - presentPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                playerCoins = shop.chargeForItem(playerCoins, presentPrice);
+                break;
+            case "E":    
+                playerCoins = shop.chargeForItem(playerCoins, milkshakePrice);
                 break;
         }
+        }  
+
+        }
     }
-}
+
+    public class Shop
+    {
+        // properties
+        // TODO: list of items in the shop to go here E.g, List<ShopItem> Stock
+
+        // constructor
+        public Shop()
+        {
+            // TODO
+        }
+
+        // methods
+        public int chargeForItem(int playerCoins, int itemPrice)
+        {
+            if (playerCoins >= itemPrice)
+            {
+                playerCoins = playerCoins - itemPrice;
+                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining."); 
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you don't have enough coins to purchase this item!");
+            }
+
+            return playerCoins;
+        }
+
+    }
+
 
